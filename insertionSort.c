6ae2656919a63c1021b1711df4c97f205fd61e39
Mahmoud Gamal <mahgam3396@gmail.com>
2020-10-11 22:52:06 +0200

    insertionSort.c is added

---
+++ insertionSort.c
@@ -0,0 +1,18 @@
+void Insertion_Sort(int arr[], int n)
+{
+    int i = 0;
+    for(i=1; i<n; i++)
+    {
+        int hole = i;
+
+		int value = arr[hole];
+
+		while((hole>0) && (arr[hole-1]>value))
+        {
+            arr[hole] = arr[hole-1];
+            --hole;
+        }
+
+		arr[hole] = value;
+    }
+}
