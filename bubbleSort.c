47e6e896bf0279bcca47bb10fb10293d220b7ea1
Mahmoud Gamal <60486460+MahmoudGamal33@users.noreply.github.com>
2020-10-12 01:00:51 +0200

    Update bubbleSort.c

--- bubbleSort.c
+++ bubbleSort.c
@@ -3,25 +3,27 @@ void Bubble_Sort(int arr[], int n)
 	int i = 0, j = 0;
 	for(i=0; i<n; i++)
 	{
+		int flag = 0;
+
 		for(j=0; j<(n-i-1); j++)
 		{
-			int flag = 0;
-			if(arr[j] < arr[j+1]
+			if(arr[j] > arr[j+1])
 			{
 				int temp = arr[j];
 				arr[j] = arr[j+1];
 				arr[j+1] = temp;
-				
-				flag = 1;	
+
+				flag = 1;
+			}
 		}
-		
+
 		if(!flag)
 		{
 			break;
 		}
 		else
 		{
-			//
+		    //
 		}
 	}
 }
